b2f0c13b985e7fccb583c8802f43b6b6bcc875d5
HongShi2333 <1986977075@qq.com>
2024-06-18 18:22:40 +0800

    new

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/17/同学板块/","2024/06/15/更新日志/","2024/06/17/老师板块/"];function toRandomPost(){
+var posts=["2024/06/15/更新日志/","2024/06/17/同学板块/","2024/06/17/老师板块/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
